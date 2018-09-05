3c0ef50389bf90693893b92d8ba62d0e0b02b1fe
alumno <alumno@LAB00PC00>
2018-09-04 22:10:58 -0300

    Ejercicio sin terminar

--- clase_5/Clase_5/main.c
+++ clase_5/Clase_5/main.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #define QUANTITY 10
 
+void loadList(int note[], int longNote);
+int returnTotal(int note[], int longList, int maxResult);
+int returnAverage(int note[], int longNote, int aveResult);
+void returnAverageInfo(int noteList[], int QUANTITY, int aveResult, int resultNumberMax);
+void returnTotalInfo(int note[], int QUANTITY, int maxResultTotal);
 
 
 int main()
 {
 
     int noteList[QUANTITY];
-    int newNumber;
-    int i;
-    int numberMax;
-    int numberMin;
-    int acumulator = 0;
-    int average;
 
+     loadList(noteList, QUANTITY);
+    returnTotalInfo(noteList, QUANTITY, maxResultTotal);
+    returnAverageInfo(noteList, QUANTITY, aveResult, resultNumberMax);
 
     /*
     printf("tamaño: %d",sizeof(noteList)); //size of determina el tamaño de la variable en bytes
@@ -23,51 +25,92 @@ int main()
     printf("direccion: %d", noteList); // la direccion de memoria de la variable
     printf("direccion: %d", noteList[0]);*/
 
-    for(i=0;i<QUANTITY;i++) {
 
-        printf("\n Ingrese nota %d: ", i+1);
+}
+
+void returnAverageInfo(int noteList[], int QUANTITY, int aveResult, int resultNumberMax) {
+
+
+    returnAverage(noteList[],QUANTITY, &aveResult, &resultNumberMax)
+    printf("Su promedio es: %d", aveResult);
+    printf("Su numero maximo es: %d", resultNumberMax);
+
+}
+
+void returnTotalInfo(int noteList, int QUANTITY, int maxResultTotal)
+{
+    returnTotal(noteList[], QUANTITY, &maxResultTotal)
+    printf("El retorno total es: %d", maxResultTotal);
+
+}
+
+void loadList (int note[], int longNote) {
+
+int i;
+int newNumber;
+
+for(i=0; i< longNote; i++) {
+
+
+printf("\n Ingrese nota %d: ", i+1);
         scanf("%d", &newNumber);
-        newNumber = noteList[i];
+        newNumber = longNote[i];
+
+}
+
+}
+
+
+
+int returnTotal(int note[], int longList, int *maxResult) {
+
+    int i;
+    int acumulator = 0;
+
+    for(i=0;i<longList;i++) {
+
+           acumulator=acumulator+note[i];
+            *maxResult = acumulator;
+
         }
 
-        for(i=0;i<QUANTITY;i++ )
-        {
-            printf("\n Sus notas son %d: ", noteList[i]);
-        }
 
 
-         for(i=0;i<QUANTITY;i++ ) {
+    return 0;
+}
 
-            if(i==0 || noteList[i] > numberMax) {
 
-                numberMax=noteList[i];
+int returnAverage(int note[], int longNote, int *aveResult) {
 
+int sum;
+int average;
+
+sum= returnTotal(note, longNote);
+average= sum/average;
+
+*aveResult = average;
+
+return 0;
+
+}
+
+int numberMax(int note[], int longNote, int *resultNumberMax) {
+
+        int numberM;
+        int i;
+
+         for(i=0;i<longNote;i++ ) {
+
+            if(i==0 || note[i] > numberM) {
+
+                numberM=note[i];
+                *resultNumberMax= numberM;
             }
         }
 
-        for(i=0;i<QUANTITY;i++ ) {
-
-            if(i==0 || noteList[i] < numberMin) {
-
-                numberMin= noteList[i];
-            }
-
-        }
-
-        for(i=0;i<QUANTITY;i++) {
-
-            acumulator=acumulator+noteList[i];
-
-
-        }
-
-        average=acumulator/QUANTITY;
-
-
-        printf("\n El numero mayor es: %d - El numero minimo es: %d \n - Su suma de notas es: %d - El promedio es: %d", numberMax, numberMin, acumulator, average);
 
 
         return 0;
-}
 
+}
 
