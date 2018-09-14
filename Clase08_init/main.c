f68c56fe3af71c5f859337cba1e6b2b06800ab85
NicolasFischetti <36281576+NicolasFischetti@users.noreply.github.com>
2018-09-13 22:01:37 -0300

    Add files via upload

--- Clase08_init/main.c
+++ Clase08_init/main.c
@@ -6,18 +6,78 @@
 
 #define CANTIDAD_EMPLEADOS 6
 
+void ordenarPorNombre(char* pNombres[] [50], char* pApellidos[] [50], char* pDNI [] [50], int limite) {
+    // hay que pasarle un array de string y caracteres, hay que pasarle la cantidad
+    int swap;
+    int i;
+    char bufferStr[250];
+
+    do {
+
+        swap = 0;
+
+        for(i=0; i < limite-1;i++){
+
+            if(strcmp(pNombres[i],pNombres[i+1]) > 0) {
+
+                swap = 1;
+
+                strcpy(bufferStr, pNombres[i]);
+                strcpy(pNombres[i], pNombres[i+1]);
+                strcpy(pNombres[i+1], bufferStr);
+
+                strcpy(bufferStr, pApellidos[i]);
+                strcpy(pApellidos[i], pApellidos[i+1]);
+                strcpy(pApellidos[i+1], bufferStr);
+
+                strcpy(bufferStr, pDNI[i]);
+                strcpy(pDNI[i], pDNI[i+1]);
+                strcpy(pDNI[i+1], bufferStr);
+
+            }
+        }
+
+    }while(swap);
+
+}
+
 int main()
 {
-    char nombres[CANTIDAD_EMPLEADOS][50];
+    char nombres[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
+    char apellidos[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
+    char dni[CANTIDAD_EMPLEADOS][50] = {"AAA", "CCC", "BBB"};
     int i;
 
-    for(i=0;i < CANTIDAD_EMPLEADOS;i++)
+
+    ordenarPorNombre(nombres,apellidos,dni,CANTIDAD_EMPLEADOS);
+    /*int j;
+    int aux;
+
+    for(i=0; i < CANTIDAD_EMPLEADOS-1;i++) {
+
+        for(j=i+1; j < CANTIDAD_EMPLEADOS;i++) {
+
+            if(strcmp(nombres[i],nombres[j])==0)
+            {
+                 strncpy()
+
+            }
+
+
+        }
+
+    }*/
+
+
+
+
+   /* for(i=0;i < CANTIDAD_EMPLEADOS;i++)
     {
             utn_getNombre(nombres[i],50,"Ingrese su nombre: ","\ndato invalido. Prueba nuevamente: ",2);
-    }
+    }*/
     for(i=0;i<CANTIDAD_EMPLEADOS;i++)
     {
-        printf("%s\n",nombres[i]);
+        printf("%s %s %s",nombres[i], apellidos[i], dni[i]);
     }
     return 0;
 }
